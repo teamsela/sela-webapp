dc60ff9f42e40cae23990c37ed3628720b16ed17
hzl0410 <hzl0410@students.cs.ubc.ca>
2024-02-08 13:16:08 -0800

    fixed spacing

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -82,7 +82,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
     <aside
       ref={sidebar}
       className={`absolute left-0 top-0 z-9999 flex h-screen w-72.5 flex-col overflow-y-hidden bg-black duration-300 ease-linear dark:bg-boxdark lg:static ${
-        sidebarOpen ? "translate-x-0" : "-translate-x-full"
+        sidebarOpen ? "translate-x-0" : "-translate-x-full hidden"
       }`}
     >
       {/* <!-- SIDEBAR HEADER --> */}
