76e08abf970db1654c4d1246f98070eb6ed9b1e2
hzl0410 <hzl0410@students.cs.ubc.ca>
2024-02-13 00:44:24 -0800

    image size for mobile

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -2,6 +2,8 @@
 
 import { SignUp } from '@clerk/nextjs';
 import React, {useState, useEffect} from "react";
+import Image from 'next/image'
+
 
 
 export default function Page() {
@@ -21,7 +23,7 @@ export default function Page() {
     })
     return (
       <div className={`min-h-screen justify-center items-center ${isMobile? "flex-row" : "flex"}`}>
-        <img src="/images/digging-for-gems.png" width="40%" style={{marginRight: 100}} className={`${isMobile ? "ml-30 my-10" : ""}`} />
+        <Image src="/images/digging-for-gems.png" width={isMobile? 150:768} alt="Bible Poetry Logo" style={{marginRight: 100}} className={`${isMobile ? "ml-30 my-10" : ""}`} />
         <div className="flex w-full flex-1 justify-center gap-2 px-8 sm:max-w-md">
           <SignUp />
         </div>
