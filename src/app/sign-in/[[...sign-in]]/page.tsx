bc146fb4019693210ae82c847cda8a5563b9ef9c
hzl0410 <hzl0410@students.cs.ubc.ca>
2024-02-12 23:43:41 -0800

    moer style

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -23,8 +23,8 @@ export default function SignInPage() {
     return (
       <main>
         <div className={`min-h-screen justify-center items-center ${isMobile? "flex-row" : "flex"} `}>
-          <img src="/images/digging-for-gems.png" width="40%"  className={`${isMobile ? "ml-20 my-10" : "mr-100"}`} />
-          <div className="flex w-full flex-1 flex-col justify-center gap-2 px-8 sm:max-w-md">
+          <img src="/images/digging-for-gems.png" width="40%"  className={`${isMobile ? "ml-30 my-10" : "mr-100"}`} />
+          <div className="flex w-full flex-1 justify-center gap-2 px-8 sm:max-w-md">
             <SignIn />
           </div>
         </div>
