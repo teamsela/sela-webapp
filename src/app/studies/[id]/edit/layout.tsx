b1f9c1ca8e7c6ac9615b72ff0e89c087f21322af
hzl0410 <hzl0410@students.cs.ubc.ca>
2024-02-15 01:04:35 -0800

    fixed build problem in edit/layout

--- src/app/studies/[id]/edit/layout.tsx
+++ src/app/studies/[id]/edit/layout.tsx
@@ -5,11 +5,13 @@ import Loader from "@/components/common/Loader";
 
 import Sidebar from "@/components/Sidebar";
 import Header from "@/components/Header";
+import NewStudyModal from "@/components/Modals/NewStudy";
+
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 
   const [sidebarOpen, setSidebarOpen] = useState(false);
-
+  const [createStudyOpen, setCreateStudyOpen] = useState(false)
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
@@ -18,38 +20,43 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
   return (
 
-        <div className="dark:bg-boxdark-2 dark:text-bodydark">
-          {loading ? (
-            <Loader />
-          ) : (
-            <div className="flex h-screen overflow-hidden">
-              {/* <!-- ===== Sidebar Start ===== --> */}
-              <Sidebar
-                sidebarOpen={sidebarOpen}
-                setSidebarOpen={setSidebarOpen}
-              />
-              {/* <!-- ===== Sidebar End ===== --> */}
+    <div className="dark:bg-boxdark-2 dark:text-bodydark">
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="flex h-screen overflow-hidden">
+          {/* <!-- ===== Sidebar Start ===== --> */}
+          <Sidebar
+            sidebarOpen={sidebarOpen}
+            setSidebarOpen={setSidebarOpen}
+            createStudyOpen={createStudyOpen}
+            setCreateStudyOpen={setCreateStudyOpen}
+          />
+          {/* <!-- ===== Sidebar End ===== --> */}
 
-              {/* <!-- ===== Content Area Start ===== --> */}
-              <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-                {/* <!-- ===== Header Start ===== --> */}
-                { <Header
-                  sidebarOpen={sidebarOpen}
-                  setSidebarOpen={setSidebarOpen}
-                /> }
-                {/* <!-- ===== Header End ===== --> */}
+          {/* <!-- ===== Content Area Start ===== --> */}
+          <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+            {/* <!-- ===== Header Start ===== --> */}
+            {<Header
+              sidebarOpen={sidebarOpen}
+              setSidebarOpen={setSidebarOpen}
+            />}
+            {/* <!-- ===== Header End ===== --> */}
 
-                {/* <!-- ===== Main Content Start ===== --> */}
-                <main>
-                  <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-                    {children}
-                  </div>
-                </main>
-                {/* <!-- ===== Main Content End ===== --> */}
+            {/* <!-- ===== Main Content Start ===== --> */}
+            <main>
+              <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                {children}
               </div>
-              {/* <!-- ===== Content Area End ===== --> */}
-            </div>
-          )}
+            </main>
+            {/* <!-- ===== Main Content End ===== --> */}
+             {/* <!-- ===== Create Study Modal Start ===== --> */}
+             <NewStudyModal open={createStudyOpen} setOpen={setCreateStudyOpen} />
+            {/* <!-- ===== Create Study Modal End ===== --> */}
+          </div>
+          {/* <!-- ===== Content Area End ===== --> */}
         </div>
+      )}
+    </div>
   );
 }
