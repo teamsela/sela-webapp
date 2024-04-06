50dce6144c819aebc50be8cee36ee0f99aaf8b58
teamsela <147948027+teamsela@users.noreply.github.com>
2024-04-06 11:35:56 -0700

    Add drop down user button in the header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -56,7 +56,7 @@ const Header = ({
           </ul>
 
           {/* <!-- User Area --> */}
-          {/*<DropdownUser /> */}
+          {<DropdownUser /> }
           {/* <!-- User Area --> */}
         </div>
       </div>
