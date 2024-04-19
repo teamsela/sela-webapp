9eb4299ca96fb3d9b148905d0ac638c31221afb9
teamsela <147948027+teamsela@users.noreply.github.com>
2024-04-19 00:16:33 -0700

    Widen the screen and reduce the horizontal padding in editor mode

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -22,7 +22,7 @@ const Editor = ({
         <Header studyName={study.name} studyPassage={study.passage} isHebrew={isHebrew} setLangToHebrew={setHebrew} />
         <Toolbar zoomLevel={zoomLevel} setZoomLevel={setZoomLevel} />
         <main>
-          <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+          <div className="mx-auto max-w-screen-3xl p-2 md:p-4 2xl:p-6">
             <Passage content={content} isHebrew={isHebrew} zoomLevel={zoomLevel} />
           </div>
         </main>
