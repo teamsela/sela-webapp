57306184bdc78c118383f2c40a944f4360fa8d4b
stoneYuan3 <jackyuan110@gmail.com>
2024-04-23 16:10:11 -0700

    progress: color function: add background color fill,
    feature works now but I cant edit individual words, every time I change colour the entire line is affected
    
    considering: create a new component for each individual words

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -16,21 +16,35 @@ const Editor = ({
   }) => {
     const [isHebrew, setHebrew] = useState(false);
     const [zoomLevel, setZoomLevel] = useState(4);
+
+    const [colorPanelActive, setColorPanelActive] = useState(false);
     
-    const [colorFill, setColorFill] = useState( { r:'255', g:'255', b:'255', a:'1' } )
+    const [colorFill, setColorFill] = useState( { r:255, g:255, b:255, a:1 } )
+    //todo: assign border and text color to their corresponding buttons in Buttons.tsx
+    const [borderColor, setBorderColor] = useState( { r:150, g:150, b:150, a:1 } )
+    const [textColor, setTextColor] = useState( { r:150, g:150, b:150, a:1 } )
 
     return (
         <>
         <Header study={study} isHebrew={isHebrew} setLangToHebrew={setHebrew} />
         <Toolbar 
           zoomLevel={zoomLevel} 
-          setZoomLevel={setZoomLevel} 
+          setZoomLevel={setZoomLevel}
+          
+          colorPanelActive={colorPanelActive}
+          setColorPanelActive={setColorPanelActive}
           colorFill={colorFill}
           setColorFill={setColorFill}
         />
         <main>
           <div className="mx-auto max-w-screen-3xl p-2 md:p-4 2xl:p-6">
-            <Passage content={content} isHebrew={isHebrew} zoomLevel={zoomLevel} />
+            <Passage 
+              content={content} 
+              isHebrew={isHebrew} 
+              zoomLevel={zoomLevel} 
+              colorFill={colorFill}
+              colorPanelActive={colorPanelActive}
+            />
           </div>
         </main>
         </>
