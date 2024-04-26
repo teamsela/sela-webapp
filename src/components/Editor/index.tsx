677a5685f888cd7b38aa6f8c9be4690335c9eda4
teamsela <147948027+teamsela@users.noreply.github.com>
2024-04-26 01:19:22 -0700

    Add more space to the right margin of horizontal overflow passage

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -22,7 +22,7 @@ const Editor = ({
         <Header study={study} isHebrew={isHebrew} setLangToHebrew={setHebrew} />
         <Toolbar zoomLevel={zoomLevel} setZoomLevel={setZoomLevel} />
         <main>
-          <div className="mx-auto max-w-screen-3xl p-2 md:p-4 2xl:p-6 overflow-x-auto whitespace-nowrap mr-4">
+          <div className="mx-auto max-w-screen-3xl p-2 md:p-4 2xl:p-6 overflow-x-auto whitespace-nowrap mr-8">
             <Passage content={content} isHebrew={isHebrew} zoomLevel={zoomLevel} />
           </div>
         </main>
