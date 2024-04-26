5eb4d924d45269baf6fe3657b0761900e9791a5f
teamsela <147948027+teamsela@users.noreply.github.com>
2024-04-26 09:37:15 -0700

    Fixed the css style of verse num again

--- src/components/Editor/Passage/index.tsx
+++ src/components/Editor/Passage/index.tsx
@@ -1,61 +1,20 @@
 import { HebWord, PassageData } from '@/lib/data';
 
-function GetBlockTextSize(zoomLevel: number) : string {
-  
-  let blockTextSize : string;
-
-  switch (zoomLevel) {
-    case 0: {
-      blockTextSize = "text-5xs";
-      break;
-    }
-    case 1: {
-      blockTextSize = "text-4xs";
-      break;
-    }
-    case 2: {
-      blockTextSize = "text-3xs";
-      break;
-    }
-    case 3: {
-      blockTextSize = "text-2xs";
-      break;
-    }
-    case 4: {
-      blockTextSize = "text-xs";
-      break;
-    }
-    case 5: {
-      blockTextSize = "text-sm";
-      break;
-    }
-    case 6: {
-      blockTextSize = "text-base";
-      break;
-    }
-    case 7: {
-      blockTextSize = "text-lg";
-      break;
-    }
-    case 8: {
-      blockTextSize = "text-xl";
-      break;
-    }
-    case 9: {
-      blockTextSize = "text-2xl";
-      break;
-    }
-    case 10: {
-      blockTextSize = "text-3xl";
-      break;
-    }    
-    default: {
-      blockTextSize = "text-base";
-      break;
-    }
-  }
-
-  return blockTextSize;
+type ZoomLevel = {
+  [level: number]: { fontSize: string, verseNumMr: string };
+}
+const zoomLevelMap : ZoomLevel = {
+  0:  { fontSize: "text-4xs",  verseNumMr: "mr-0.5" },
+  1:  { fontSize: "text-4xs",  verseNumMr: "mr-0.5" },
+  2:  { fontSize: "text-3xs",  verseNumMr: "mr-0.5" },
+  3:  { fontSize: "text-2xs",  verseNumMr: "mr-0.5" },
+  4:  { fontSize: "text-xs",   verseNumMr: "mr-0.5" },
+  5:  { fontSize: "text-sm",   verseNumMr: "mr-1"   },
+  6:  { fontSize: "text-base", verseNumMr: "mr-1" },
+  7:  { fontSize: "text-lg",   verseNumMr: "mr-1" },
+  8:  { fontSize: "text-xl",   verseNumMr: "mr-1" },
+  9:  { fontSize: "text-2xl",  verseNumMr: "mr-2" },
+  10: { fontSize: "text-3xl",  verseNumMr: "mr-2" },
 }
 
 const ParagraphContent = ({ isHebrew, zoomLevel, verseNumber, content } : { 
@@ -65,17 +24,15 @@ const ParagraphContent = ({ isHebrew, zoomLevel, verseNumber, content } : {
   content: HebWord[];
 }) => {
 
-  const verseNumFontSize : string = GetBlockTextSize(isHebrew && zoomLevel >= 2 ? zoomLevel-2 : zoomLevel-1);
-
   const verseNumStyles = {
     container: {
-      className: `font-features sups ${verseNumFontSize} ${isHebrew ? "w-" + Math.floor(zoomLevel/3) + " ml-2" : "w-1 mr-" + Math.ceil(zoomLevel/5)}`
+      className: `font-features sups ${isHebrew ? "w-1 ml-2" : "w-1 " + zoomLevelMap[zoomLevel].verseNumMr}`
     }
   }  
   return (
     content.map((word, index) => (
         <span key={word.id} className="flex items-center justify-center rounded border select-none px-2 py-1 text-center hover:opacity-60">
-          {/*paragraphIndex === 0 &&*/ index === 0 ? <sup {...verseNumStyles.container}>{verseNumber}</sup> : "" }
+          {index === 0 ? <sup {...verseNumStyles.container}>{verseNumber}</sup> : "" }
           {!isHebrew ? word.gloss : word.wlcWord}
         </span>
     ))
@@ -92,7 +49,7 @@ const Passage = ({
 
   const styles = {
     container: {
-      className: `flex gap-2 mb-2 ${isHebrew ? "hbFont " : ""}${GetBlockTextSize(zoomLevel)}`
+      className: `flex gap-1.5 mb-1.5 ${isHebrew ? "hbFont " : ""}${zoomLevelMap[zoomLevel].fontSize}`
     }
   }
 
