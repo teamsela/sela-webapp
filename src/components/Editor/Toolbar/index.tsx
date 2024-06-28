dc51eaa7cdc3697e12995300af3921937a33bf7e
ywa416 <ywa416@sfu.ca>
2024-06-28 16:24:05 -0700

    refact: render buttons in toolbar

--- src/components/Editor/Toolbar/index.tsx
+++ src/components/Editor/Toolbar/index.tsx
@@ -32,42 +32,48 @@ const Toolbar = ({
       setColorAction(ColorActionType.none);
   }, [ctxNumSelectedWords])
 
-  /* TODO: refactor this part after more features are added to view mode*/
-  if (inViewMode) { // only shows zoom buttons if in view-only mode
-    return (
-      <div className="sticky left-0 top-20 z-9999 flex w-full drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
-        <div className="mx-auto my-2 grid max-w-80 bg-white grid-cols-3 rounded-md border border-stroke py-2 shadow-1 dark:border-strokedark dark:bg-[#37404F]" style={{position:"relative"}}>
-          <ZoomOutBtn zoomLevel={ctxZoomLevel} setZoomLevel={setZoomLevel} />
-          <div className="flex flex-col group relative inline-block items-center justify-center xsm:flex-row">
-            <span className="rounded-md border-[.5px] text-center mr-3 border-stroke bg-gray-2 px-4 py-0.5 text-base font-medium text-black dark:border-strokedark dark:bg-boxdark-2 dark:text-white 2xl:ml-4">
-            {ctxZoomLevel}
-            </span>
-          </div>
-          <ZoomInBtn zoomLevel={ctxZoomLevel} setZoomLevel={setZoomLevel} />
+  /* TODO: may need to refactor this part after more features are added to view mode*/
+  return (
+    <div className="sticky left-0 top-20 z-9999 flex w-full drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
+    <div className="mx-auto my-2 max-w-180 bg-white rounded-md border border-stroke py-2 shadow-1 dark:border-strokedark dark:bg-[#37404F]" style={{position:"relative"}}>
+    { // only show zoom in/out buttons in view only mode
+      inViewMode
+      ? (<div className="grid grid-cols-3">
+        <ZoomOutBtn zoomLevel={ctxZoomLevel} setZoomLevel={setZoomLevel} />
+        <div className="flex flex-col group relative inline-block items-center justify-center xsm:flex-row">
+          <span className="rounded-md border-[.5px] text-center mr-3 border-stroke bg-gray-2 px-4 py-0.5 text-base font-medium text-black dark:border-strokedark dark:bg-boxdark-2 dark:text-white 2xl:ml-4">
+          {ctxZoomLevel}
+          </span>
         </div>
-      </div>
-    );
-  } else {
-    return (
-      <div className="sticky left-0 top-20 z-9999 flex w-full drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
-        <div className="mx-auto my-2 grid max-w-180 bg-white grid-cols-16 rounded-md border border-stroke py-2 shadow-1 dark:border-strokedark dark:bg-[#37404F]" style={{position:"relative"}}>
-          <UndoBtn />
-          <RedoBtn />
-          <ColorFillBtn setColor={setColorFill} setColorAction={setColorAction}/>
-          <BorderColorBtn setColor={setBorderColor} setColorAction={setColorAction}/>
-          <TextColorBtn setColor={setTextColor} setColorAction={setColorAction}/>
-          <ClearFormatBtn resetColorFill={setColorFill} resetBorderColor={setBorderColor} resetTextColor={setTextColor} setColorAction={setColorAction} />
-          <UniformWidthBtn setUniformWidth={setUniformWidth}/>
-          <LeftIndentBtn />
-          <RightIndentBtn />
-          <NewStropheBtn />
-          <NewStanzaBtn />
-          <MoveUpBtn />
-          <MoveDownBtn />
+        <ZoomInBtn zoomLevel={ctxZoomLevel} setZoomLevel={setZoomLevel} />
+      </div>)
+      : (<div className="grid grid-cols-16">
+        {/* {ctxNumSelectedWords ? <DeselectAllBtn /> : ""} */}
+        <UndoBtn />
+        <RedoBtn />
+        <ZoomOutBtn zoomLevel={ctxZoomLevel} setZoomLevel={setZoomLevel} />
+        <div className="flex flex-col group relative inline-block items-center justify-center xsm:flex-row">
+          <span className="rounded-md border-[.5px] text-center mr-3 border-stroke bg-gray-2 px-4 py-0.5 text-base font-medium text-black dark:border-strokedark dark:bg-boxdark-2 dark:text-white 2xl:ml-4">
+          {ctxZoomLevel}
+          </span>
         </div>
-      </div>
-    );
-  }
+        <ZoomInBtn zoomLevel={ctxZoomLevel} setZoomLevel={setZoomLevel} />
+        <ColorFillBtn setColor={setColorFill} setColorAction={setColorAction}/>
+        <BorderColorBtn setColor={setBorderColor} setColorAction={setColorAction}/>
+        <TextColorBtn setColor={setTextColor} setColorAction={setColorAction}/>
+        <ClearFormatBtn resetColorFill={setColorFill} resetBorderColor={setBorderColor} resetTextColor={setTextColor} setColorAction={setColorAction} />
+        <UniformWidthBtn setUniformWidth={setUniformWidth}/>
+        <LeftIndentBtn />
+        <RightIndentBtn />
+        <NewStropheBtn />
+        <NewStanzaBtn />
+        <MoveUpBtn />
+        <MoveDownBtn />
+      </div>)
+    }
+  </div>
+  </div>
+  );
 };
 
 
