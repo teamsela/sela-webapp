def1a87db852c6f8d9c7ff5a1b32e46472b46adf
ywa416 <ywa416@sfu.ca>
2024-07-29 17:40:45 -0700

    fix: update recent table search placeholder message

--- src/components/Tables/Recent.tsx
+++ src/components/Tables/Recent.tsx
@@ -44,7 +44,7 @@ export default async function RecentTable({
 
   return (
     <>
-    <SearchBar placeholder="Search study..." />
+    <SearchBar placeholder="Search study by name or passage..." />
     <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
       <div className="max-w-full overflow-x-auto">
         <table className="w-full table-auto">
