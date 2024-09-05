40e33997b8b85ba72393ec3e4e77771f67c784b6
teamsela <147948027+teamsela@users.noreply.github.com>
2024-09-05 00:38:15 -0700

    Removed undo/redo button until next launch and fix the tooltip for zoom in/out buttons

--- src/components/StudyPane/Toolbar/index.tsx
+++ src/components/StudyPane/Toolbar/index.tsx
@@ -28,32 +28,28 @@ const Toolbar = ({
       { // only show zoom in/out & uniform width buttons in view only mode
         ctxInViewMode
         ? (<div className="flex">
-          <div className="flex flex-row group">
             <ZoomOutBtn zoomLevel={ctxZoomLevel} setZoomLevel={setZoomLevel} />
             <div className="flex flex-col group relative inline-block items-center justify-center xsm:flex-row">
-              <span className="rounded-md border-[.5px] text-center border-stroke bg-gray-2 px-4 py-0.5 text-base font-medium text-black dark:border-strokedark dark:bg-boxdark-2 dark:text-white">
+              <span className="rounded-md border-[.5px] text-center ml-3 border-stroke bg-gray-2 px-4 py-0.5 text-base font-medium text-black dark:border-strokedark dark:bg-boxdark-2 dark:text-white">
               {ctxZoomLevel}
               </span>
             </div>
             <ZoomInBtn zoomLevel={ctxZoomLevel} setZoomLevel={setZoomLevel} />
             <UniformWidthBtn setUniformWidth={setUniformWidth}/>
-          </div>
         </div>)
         : (<div className="flex">
-          <div className="flex flex-row group">
+          {/*<div className="flex flex-row group">
             <UndoBtn />
             <RedoBtn />
-          </div>
+          </div>*/}
 
-          <div className="flex flex-row group">
-            <ZoomOutBtn zoomLevel={ctxZoomLevel} setZoomLevel={setZoomLevel} />
-            <div className="flex flex-col group relative inline-block items-center justify-center xsm:flex-row">
-              <span className="rounded-md border-[.5px] text-center mr-3 border-stroke bg-gray-2 px-4 py-0.5 text-base font-medium text-black dark:border-strokedark dark:bg-boxdark-2 dark:text-white 2xl:ml-4">
-              {ctxZoomLevel}
-              </span>
-            </div>
-            <ZoomInBtn zoomLevel={ctxZoomLevel} setZoomLevel={setZoomLevel} />
+          <ZoomOutBtn zoomLevel={ctxZoomLevel} setZoomLevel={setZoomLevel} />
+          <div className="flex flex-col group relative inline-block items-center justify-center xsm:flex-row">
+            <span className="rounded-md border-[.5px] text-center ml-2 border-stroke bg-gray-2 px-4 py-0.5 text-base font-medium text-black dark:border-strokedark dark:bg-boxdark-2 dark:text-white">
+            {ctxZoomLevel}
+            </span>
           </div>
+          <ZoomInBtn zoomLevel={ctxZoomLevel} setZoomLevel={setZoomLevel} />
 
           <ColorActionBtn colorAction={ColorActionType.colorFill} setColorAction={setColorAction} setSelectedColor={setSelectedColor}/>
           <ColorActionBtn colorAction={ColorActionType.borderColor} setColorAction={setColorAction} setSelectedColor={setSelectedColor}/>
