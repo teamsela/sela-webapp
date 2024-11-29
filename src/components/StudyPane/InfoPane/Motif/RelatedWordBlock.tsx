b95a277131a874d3e1bb3f1b38814a71f7df1cad
ywa416 <ywa416@sfu.ca>
2024-11-28 17:20:21 -0800

    cleanup code, remove smart highlight button

--- src/components/StudyPane/InfoPane/Motif/RelatedWordBlock.tsx
+++ src/components/StudyPane/InfoPane/Motif/RelatedWordBlock.tsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useState, useContext } from 'react';
 
 import { DEFAULT_COLOR_FILL, DEFAULT_BORDER_COLOR, DEFAULT_TEXT_COLOR, FormatContext } from '../../index';
-import { ColorActionType } from "@/lib/types";
 import { HebWord, LexiconData } from '@/lib/data';
 
 export const RelatedWordBlock = ({
@@ -13,59 +12,8 @@ export const RelatedWordBlock = ({
   relatedWords: HebWord[]
 }) => {
 
-  const { ctxRelWordsColorMap, ctxColorAction, ctxSelectedColor, ctxSelectedHebWords, ctxSetNumSelectedWords, ctxSetSelectedHebWords } = useContext(FormatContext)
-  
-  let defaultColorFill = relatedWords[0].colorFill || DEFAULT_COLOR_FILL;
-  let defaultBorderColor = relatedWords[0].borderColor || DEFAULT_BORDER_COLOR;
-  let defaultTextColor = relatedWords[0].textColor || DEFAULT_TEXT_COLOR;
-
-  const relBlockColor = ctxRelWordsColorMap.get(rootData.strongCode);
-  if (relBlockColor) {
-    defaultColorFill = relBlockColor.colorFill;
-    defaultTextColor = relBlockColor.textColor;
-  }
-
-  relatedWords.forEach((dsd) => {
-    if (dsd.colorFill !== defaultColorFill) { defaultColorFill = DEFAULT_COLOR_FILL; }
-    if (dsd.borderColor !== defaultBorderColor) { defaultBorderColor = DEFAULT_BORDER_COLOR; }
-    if (dsd.textColor !== defaultTextColor) { defaultTextColor = DEFAULT_TEXT_COLOR; }
-  });
-
-  const [colorFillLocal, setColorFillLocal] = useState(defaultColorFill);
-  const [borderColorLocal, setBorderColorLocal] = useState(defaultBorderColor);
-  const [textColorLocal, setTextColorLocal] = useState(defaultTextColor);
+  const { ctxSelectedHebWords, ctxSetNumSelectedWords, ctxSetSelectedHebWords } = useContext(FormatContext)
   const [selected, setSelected] = useState(false);
-
-  useEffect(() => {
-    const relBlockColor = ctxRelWordsColorMap.get(rootData.strongCode);
-    if (relBlockColor) {
-      setColorFillLocal(relBlockColor.colorFill);
-      setTextColorLocal(relBlockColor.textColor);
-    }
-  }, [ctxRelWordsColorMap]);
-  
-  useEffect(() => {
-    const colorFill = relatedWords[0].colorFill;
-    const isSameColorFill = relatedWords.every((x) => x.colorFill === colorFill);
-    if (!isSameColorFill) {
-       setColorFillLocal(DEFAULT_COLOR_FILL);
-    } else if (colorFill != colorFillLocal) { setColorFillLocal(colorFill); }
-
-    const textColor = relatedWords[0].textColor;
-    const isSameTextColor = relatedWords.every((x) => x.textColor === textColor);
-    if (!isSameTextColor) {
-       setTextColorLocal(DEFAULT_TEXT_COLOR);
-    } else if (textColor != textColorLocal) { setTextColorLocal(textColor); }
-  }, [relatedWords]);
-
-  useEffect(() => {
-    let hasChildren = true;
-    relatedWords.forEach((dsd) => {
-      hasChildren = hasChildren && ctxSelectedHebWords.includes(dsd);
-    })
-
-    setSelected(hasChildren);
-  }, [ctxSelectedHebWords, relatedWords]);
 
   const handleClick = (e: React.MouseEvent) => {
     setSelected(prevState => !prevState);
@@ -82,22 +30,6 @@ export const RelatedWordBlock = ({
     ctxSetNumSelectedWords(updatedSelectedHebWords.length);
   };
 
-  useEffect(() => {
-    if (ctxColorAction !== ColorActionType.none && selected) {
-      if (ctxColorAction === ColorActionType.colorFill && ctxSelectedColor) {
-        setColorFillLocal(ctxSelectedColor);
-      } else if (ctxColorAction === ColorActionType.borderColor && ctxSelectedColor) {
-        setBorderColorLocal(ctxSelectedColor);
-      } else if (ctxColorAction === ColorActionType.textColor && ctxSelectedColor) {
-        setTextColorLocal(ctxSelectedColor);
-      } else if (ctxColorAction === ColorActionType.resetColor) {
-        setColorFillLocal(DEFAULT_COLOR_FILL);
-        setBorderColorLocal(DEFAULT_BORDER_COLOR);
-        setTextColorLocal(DEFAULT_TEXT_COLOR);
-      }
-    }
-  }, [ctxColorAction, ctxSelectedColor, selected]);
-
   return (
     <div className="flex my-1">
       <div
@@ -107,9 +39,9 @@ export const RelatedWordBlock = ({
         data-clicktype="clickable"
         style={
           {
-            background: `${colorFillLocal}`,
-            border: `2px solid ${borderColorLocal}`,
-            color: `${textColorLocal}`,
+            background: `${DEFAULT_COLOR_FILL}`,
+            border: `2px solid ${DEFAULT_BORDER_COLOR}`,
+            color: `${DEFAULT_TEXT_COLOR}`,
           }
         }>
         <span
--- src/components/StudyPane/index.tsx
+++ src/components/StudyPane/index.tsx
@@ -47,8 +47,6 @@ export const FormatContext = createContext({
   ctxSetStructureUpdateType: (arg: StructureUpdateType) => {},
   ctxRootsColorMap : {} as Map<number, ColorType>,
   ctxSetRootsColorMap : (arg: Map<number, ColorType>) =>{},
-  ctxRelWordsColorMap : {} as Map<string, ColorType>,
-  ctxSetRelWordsColorMap : (arg: Map<string, ColorType>) =>{},
 });
 
 const StudyPane = ({
@@ -81,7 +79,6 @@ const StudyPane = ({
   const [infoPaneAction, setInfoPaneAction] = useState(InfoPaneActionType.none);
   const [structureUpdateType, setStructureUpdateType] = useState(StructureUpdateType.none);
   const [rootsColorMap, setRootsColorMap] = useState<Map<number, ColorType>>(new Map());
-  const [relWordsColorMap, setRelWordsColorMap] = useState<Map<string, ColorType>>(new Map());
 
   const formatContextValue = {
     ctxStudyId: study.id,
@@ -116,8 +113,6 @@ const StudyPane = ({
     ctxSetStropheCount: setStropheCount,
     ctxRootsColorMap: rootsColorMap,
     ctxSetRootsColorMap: setRootsColorMap,
-    ctxRelWordsColorMap: relWordsColorMap,
-    ctxSetRelWordsColorMap: setRelWordsColorMap,
   }
 
 
