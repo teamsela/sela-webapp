e22f3a9bb9aabc2540de22e63e77dc3461259d48
Jack Yuan <jackyuan110@gmail.com>
2024-12-09 20:12:15 -0800

    smart highlight text & icon change

--- src/components/Modals/SmartHighlight.tsx
+++ src/components/Modals/SmartHighlight.tsx
@@ -93,12 +93,35 @@ const SmartHighlight: React.FC<SmartHighlightProps> = ({rootWords}) => {
           onBlur={() => setModalOpen(false)}
           className="w-full max-w-142.5 rounded-lg bg-white px-8 py-12 text-center dark:bg-boxdark md:px-17.5 md:py-15"
         >
-          <h3 className="pb-2 text-xl font-bold text-black dark:text-white sm:text-2xl">
-            Warning!
-          </h3>
-          <span className="mx-auto mb-6 inline-block h-1 w-22.5 rounded bg-primary"></span>
+          <span className="mx-auto inline-block">
+            <svg
+              width="60"
+              height="60"
+              viewBox="0 0 60 60"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect
+                opacity="0.1"
+                width="60"
+                height="60"
+                rx="30"
+                fill="#DC2626"
+              />
+              <path
+                d="M30 27.2498V29.9998V27.2498ZM30 35.4999H30.0134H30ZM20.6914 41H39.3086C41.3778 41 42.6704 38.7078 41.6358 36.8749L32.3272 20.3747C31.2926 18.5418 28.7074 18.5418 27.6728 20.3747L18.3642 36.8749C17.3296 38.7078 18.6222 41 20.6914 41Z"
+                stroke="#DC2626"
+                strokeWidth="2.2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </span>
+          <h3 className="mt-5.5 pb-2 text-xl font-bold text-black dark:text-white sm:text-2xl">
+            Confirm Smart Highlight
+          </h3>          
           <p className="mb-10">
-            Using smart highlight will overwrite your custom styles on related words.
+            Using smart highlight will overwrite your custom styles on identical words.
           </p>
           <div className="-mx-3 flex flex-wrap gap-y-4">
             <div className="w-full px-3 2xsm:w-1/2">
