236e065ce5e84542968dbbf138cc515783494924
Alvin Chou <achou7@gmail.com>
2025-04-07 20:46:53 -0700

    Revising fix to use pointer-events-none to also solve the collapse button issue raised in Toby's testing

--- src/components/StudyPane/Toolbar/Buttons.tsx
+++ src/components/StudyPane/Toolbar/Buttons.tsx
@@ -17,7 +17,7 @@ import { updateMetadata } from "@/lib/actions";
 
 export const ToolTip = ({ text }: { text: string }) => {
   return (
-    <div className="absolute left-1/2 top-full mt-3 -translate-x-1/2 whitespace-nowrap rounded bg-black px-4.5 py-1.5 text-xs text-white opacity-0 group-hover:opacity-100 hover:!opacity-0">
+    <div className="absolute left-1/2 top-full mt-3 -translate-x-1/2 whitespace-nowrap rounded bg-black px-4.5 py-1.5 text-xs text-white opacity-0 group-hover:opacity-100 pointer-events-none">
       <span className="absolute left-1/2 top-[-3px] -z-10 h-2 w-2 -translate-x-1/2 rotate-45 rounded-sm bg-black"></span>
       {text}
     </div>
