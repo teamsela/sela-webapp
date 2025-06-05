4c23f90a84473ca27e68bd69d6cc57f8a797e80a
Xu Peng Yan <xupeng.yan@gmail.com>
2025-06-04 23:40:38 -0700

    progress save

--- src/components/Tables/Premarked.tsx
+++ src/components/Tables/Premarked.tsx
@@ -76,7 +76,7 @@ export default async function PremarkedTable({
                 </td>
                 <td className="border-b border-[#eee] py-5 px-4 pl-9 dark:border-strokedark xl:pl-11">
                   <p className="text-black dark:text-white">
-                    Psalm {studyItem.passage}
+                    {studyItem.book} {studyItem.passage}
                   </p>
                 </td>
               </tr>
